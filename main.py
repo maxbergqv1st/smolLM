0e4d28424fc527a5524011e22752b45bf831f61e
maxbergqv1st <max.bergqvist@student.nbi-handelsakademin.se>
2026-05-26 11:04:52 +0200

    smolLM implementation to for llm dishes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from pprint import pprint
 
 class SmolLM:
   def __init__(self, model_name="HuggingFaceTB/SmolLM-135M-Instruct"):
-    print("Loading {model_name} inte memory (this may take a while)...")
+    print(f"Loading {model_name} into memory (this may take a while)...")
     self.pipe = pipeline("text-generation", model_name)
     print("Model loaded successfully!")
 
@@ -14,14 +14,43 @@ class SmolLM:
 
 
 class PromptTemplate:
-  def __init__():
-    pass
-  
-  def format():
-    pass
+  def __init__(self, template_str: str):
+    self.template_str = template_str
+
+  def format(self, **kwargs):
+    return self.template_str.format(**kwargs)
+    
+
+  def __or__(self, other):
+    if isinstance(other, SmolLM):
+      return LLMChain(
+        prompt_template=self,
+        llm=other
+      )
+    raise TypeError("Its not an instance of SmolLM")
+
+class LLMChain:
+  def __init__(
+      self, 
+      prompt_template: PromptTemplate, 
+      llm: SmolLM):
+    self.prompt_template = prompt_template
+    self.llm = llm
+
+  def invoke(self, **kwargs):
+    formatted_prompt = self.prompt_template.format(**kwargs)
+    return self.llm.invoke(formatted_prompt)
 
 llm = SmolLM()
 
+recipe_template = PromptTemplate(
+  template_str="Give me a quick 2-step recipe for a {dish} using only {ingredient_count} ingredients."
+)
+
+recipe_chain = recipe_template | llm
+
+result = recipe_chain.invoke(dish="Spagetti", ingredient_count="Five")
+pprint(result)
 
 # pprint(llm.invoke("what is the capital of france"))
 
